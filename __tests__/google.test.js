db02b596a4d8c87f82a2065c968ec1a9cd10ae09
linh <linhdam>
2023-08-17 16:01:39 +0700

    Test search gg

---
+++ __tests__/google.test.js
@@ -0,0 +1,40 @@
+import { render, screen } from "@testing-library/react";
+
+let puppeteer = require("puppeteer");
+let browser = null;
+let page = null;
+
+describe("google", () => {
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: false,
+      args: ["--start-fullscreen"],
+    });
+    page = await browser.newPage();
+    await page.setViewport({ width: 0, height: 0 });
+
+    jest.setTimeout(60000);
+  });
+
+  // afterAll(async () => {
+  //   await browser.close();
+  // });
+
+  beforeEach(async () => {
+    await page.goto("https://google.vn/");
+  });
+
+  it('should be titled "shopee"', async () => {
+    await expect(page.title()).resolves.toMatch("Google");
+  });
+
+  test("Test search Keyboard", async () => {
+    const xpSearchBox =
+      "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/textarea";
+    const searchBox = await page.$x(xpSearchBox);
+
+    // await page.mouse.down();
+    await searchBox[0].type("Keyboard");
+    await page.keyboard.press("Enter");
+  }, 60000);
+});
